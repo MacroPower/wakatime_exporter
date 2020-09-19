d677223651622410ffc98251e409299c80d66574
Jacob Colvin <jacobcolvin1@gmail.com>
2020-09-19 15:38:43 -0400

    Split main and collector files
    to better distinguish code from node_exporter

--- collector/collector.go
+++ collector/collector.go
@@ -1,29 +1,26 @@
-// Copyright 2015 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2015 The Prometheus Authors
+Modifications Copyright 2020 Jacob Colvin (MacroPower)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package collector
 
 import (
-	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"net/url"
-	"path"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -33,9 +30,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
-
-// Namespace defines the common namespace to be used by all metrics.
-const namespace = "wakatime"
 
 var (
 	scrapeDurationDesc = prometheus.NewDesc(
@@ -83,7 +77,9 @@ func registerCollector(collector string, isDefaultEnabled bool, factory func(in 
 	flagName := fmt.Sprintf("collector.%s", collector)
 	flagHelp := fmt.Sprintf("Enable the %s collector (default: %s).", collector, helpDefaultState)
 	defaultValue := fmt.Sprintf("%v", isDefaultEnabled)
-	envar := "WAKA_COLLECTOR_" + strings.ToUpper(collector)
+
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+	envar := "WAKA_COLLECTOR_" + strings.ToUpper(reg.ReplaceAllString(collector, ""))
 
 	flag := kingpin.Flag(flagName, flagHelp).Default(defaultValue).Envar(envar).Action(collectorFlagAction(collector)).Bool()
 	collectorState[collector] = flag
@@ -207,66 +203,4 @@ var ErrNoData = errors.New("collector returned no data")
 
 func isNoDataError(err error) bool {
 	return err == ErrNoData
-}
-
-// BoolToBinary converts booleans to "0" or "1"
-func BoolToBinary(b bool) string {
-	if b {
-		return "1"
-	}
-	return "0"
-}
-
-// FetchHTTP is a generic fetch method for Wakatime API endpoints
-func FetchHTTP(token string, sslVerify bool, timeout time.Duration, logger log.Logger) func(uri url.URL, subPath string, params url.Values) (io.ReadCloser, error) {
-	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: !sslVerify}}
-	client := http.Client{
-		Timeout:   timeout,
-		Transport: tr,
-	}
-	sEnc := base64.StdEncoding.EncodeToString([]byte(token))
-	return func(uri url.URL, subPath string, params url.Values) (io.ReadCloser, error) {
-		uri.Path = path.Join(uri.Path, subPath)
-		uri.RawQuery = params.Encode()
-		url := uri.String()
-
-		level.Info(logger).Log("msg", "Scraping Wakatime", "path", subPath, "url", url)
-
-		req, err := http.NewRequest(http.MethodGet, url, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header = map[string][]string{
-			"Authorization": {"Basic " + sEnc},
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-
-		if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
-			resp.Body.Close()
-			return nil, fmt.Errorf("HTTP status %d", resp.StatusCode)
-		}
-		return resp.Body, nil
-	}
-}
-
-// ReadAndUnmarshal reads the JSON response body and unmarshals the response
-func ReadAndUnmarshal(body io.ReadCloser, object interface{}) error {
-	respBody, readErr := ioutil.ReadAll(body)
-
-	if readErr != nil {
-		return readErr
-	}
-
-	var jsonErr error
-	jsonErr = json.Unmarshal(respBody, &object)
-	if jsonErr != nil {
-		return jsonErr
-	}
-
-	return nil
 }
--- main.go
+++ main.go
@@ -17,17 +17,12 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
-	"sort"
 
-	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/common/promlog/flag"
 	"github.com/prometheus/common/version"
@@ -35,108 +30,6 @@ import (
 
 	"github.com/MacroPower/wakatime_exporter/collector"
 )
-
-// handler wraps an unfiltered http.Handler but uses a filtered handler,
-// created on the fly, if filtering is requested. Create instances with
-// newHandler.
-type handler struct {
-	unfilteredHandler http.Handler
-	// exporterMetricsRegistry is a separate registry for the metrics about
-	// the exporter itself.
-	exporterMetricsRegistry *prometheus.Registry
-	includeExporterMetrics  bool
-	commonInputs            collector.CommonInputs
-	logger                  log.Logger
-}
-
-func newHandler(commonInputs collector.CommonInputs, includeExporterMetrics bool, logger log.Logger) *handler {
-	h := &handler{
-		exporterMetricsRegistry: prometheus.NewRegistry(),
-		includeExporterMetrics:  includeExporterMetrics,
-		commonInputs:            commonInputs,
-		logger:                  logger,
-	}
-	if h.includeExporterMetrics {
-		h.exporterMetricsRegistry.MustRegister(
-			prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
-			prometheus.NewGoCollector(),
-		)
-	}
-	if innerHandler, err := h.innerHandler(); err != nil {
-		panic(fmt.Sprintf("Couldn't create metrics handler: %s", err))
-	} else {
-		h.unfilteredHandler = innerHandler
-	}
-	return h
-}
-
-// ServeHTTP implements http.Handler.
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	filters := r.URL.Query()["collect[]"]
-	level.Debug(h.logger).Log("msg", "collect query:", "filters", filters)
-
-	if len(filters) == 0 {
-		// No filters, use the prepared unfiltered handler.
-		h.unfilteredHandler.ServeHTTP(w, r)
-		return
-	}
-	// To serve filtered metrics, we create a filtering handler on the fly.
-	filteredHandler, err := h.innerHandler(filters...)
-	if err != nil {
-		level.Warn(h.logger).Log("msg", "Couldn't create filtered metrics handler:", "err", err)
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Couldn't create filtered metrics handler: %s", err)))
-		return
-	}
-	filteredHandler.ServeHTTP(w, r)
-}
-
-// innerHandler is used to create both the one unfiltered http.Handler to be
-// wrapped by the outer handler and also the filtered handlers created on the
-// fly. The former is accomplished by calling innerHandler without any arguments
-// (in which case it will log all the collectors enabled via command-line
-// flags).
-func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
-	nc, err := collector.NewWakaCollector(h.commonInputs, h.logger, filters...)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't create collector: %s", err)
-	}
-
-	// Only log the creation of an unfiltered handler, which should happen
-	// only once upon startup.
-	if len(filters) == 0 {
-		level.Info(h.logger).Log("msg", "Enabled collectors")
-		collectors := []string{}
-		for n := range nc.Collectors {
-			collectors = append(collectors, n)
-		}
-		sort.Strings(collectors)
-		for _, c := range collectors {
-			level.Info(h.logger).Log("collector", c)
-		}
-	}
-
-	r := prometheus.NewRegistry()
-	r.MustRegister(version.NewCollector("wakatime_exporter"))
-	if err := r.Register(nc); err != nil {
-		return nil, fmt.Errorf("couldn't register collector: %s", err)
-	}
-	handler := promhttp.HandlerFor(
-		prometheus.Gatherers{h.exporterMetricsRegistry, r},
-		promhttp.HandlerOpts{
-			ErrorHandling: promhttp.ContinueOnError,
-			Registry:      h.exporterMetricsRegistry,
-		},
-	)
-	if h.includeExporterMetrics {
-		// Note that we have to use h.exporterMetricsRegistry here to
-		// use the same promhttp metrics for all expositions.
-		handler = promhttp.InstrumentMetricHandler(
-			h.exporterMetricsRegistry, handler,
-		)
-	}
-	return handler, nil
-}
 
 // UserPath appends the User path to a given URL
 func UserPath(uri *url.URL, user string) url.URL {
